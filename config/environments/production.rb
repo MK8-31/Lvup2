6f163b034248c9cfa69c389005cc17bbbbf7c872
MK8-31 <popofst@yahoo.co.jp>
2021-07-23 11:43:14 +0900

    bug fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
     password: ENV['GMAIL_PASSWORD'],
     #パスワードをBase64でエンコード
     authentication: :login
-
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
